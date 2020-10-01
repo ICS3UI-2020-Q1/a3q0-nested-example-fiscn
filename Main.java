72e4cf725579a59c3ee79e7fca8ba2a9cb4874aa
fiscn <fiscn2709@wrdsb.ca>
2020-10-01 13:46:19 +0000

    Added more examples

--- Main.java
+++ Main.java
@@ -14,8 +14,48 @@ public class Main {
     // create Scanner for user input 
     Scanner input = new Scanner(System.in); 
 
+    //Switch case example 
+    // get the user choice between 1 and 5 
+    System.out.println("please make a selection (1-5)"); 
+    int choice = input.nextInt();  
+    // gets rid of the enter key 
+    input.nextLine(); 
+
+    // Determine which candy to drop 
+    if(choice == 1){
+      System.out.println("Here is your chocolate bar"); 
+    } else if (choice == 2){
+      System.out.println("Here is your Cliff bar");
+    } 
+    
+
+    //the same thing but as a switch statement 
+    switch(choice){
+      case 1: 
+      System.out.println("here is your chocolate bar"); 
+      break;
+      case 2: 
+      System.out.println("here is your Cliff bar"); 
+      break; 
+      case 3: 
+      System.out.println("here is you M&M's"); 
+      break; 
+      default: 
+      System.out.println("invalid choice");
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Nested loop example 
     // ask the user if the animal has feathers? (yes/no)
-    System.out.println("Does the animal jave feathers?"); 
+    System.out.println("Does the animal have feathers?"); 
     String featherAnswer = input.nextLine(); 
 
     // check to see the feather statis
